3d888fe10eea7d66261ecbe7fedcadf69ddd1c7e
Manjuks155 <manjuks155@gmail.com>
2025-10-28 10:49:49 +0530

    Add files via upload

---
+++ DecisionMaking.java
@@ -0,0 +1,31 @@
+package com.techie.corejava;
+
+public class DecisionMaking {
+
+	public static void main(String[] args) {
+
+		for (int num = 1; num <= 20; num++) {
+			
+			switch (num) {
+
+			case 5:
+				System.out.println("It is 5");
+				break;
+
+			case 10:
+				System.out.println("It is 10");
+				break;
+
+			case 15:
+				System.out.println("It is 15");
+				break;
+
+			default:
+				System.out.println("this is default: " + num);
+
+			}
+		}
+
+	}
+
+}
---
+++ Identifiers_check.java
@@ -0,0 +1,11 @@
+package com.techie.corejava;
+
+public class Identifiers_check {
+	public static void main(String args[]) {
+
+		String welcome = "Welcome to Java";
+
+		System.out.println(welcome);
+
+	}
+}
