ef070c2a6b4a11aaee80e291e4f98f65e7d4bee8
Manjuks155 <manjuks155@gmail.com>
2025-10-28 10:57:04 +0530

    Add files via upload

---
+++ src/com/techie/corejava/BooleanCheck.java
@@ -0,0 +1,12 @@
+package com.techie.corejava;
+
+public class BooleanCheck {
+
+	boolean booleanInstVal;
+
+	public static void main(String args[]) {
+		BooleanCheck booleanCheck = new BooleanCheck();
+		System.out.println(booleanCheck.booleanInstVal);
+	}
+
+}
---
+++ src/com/techie/corejava/MethodAndParameterScope.java
@@ -0,0 +1,31 @@
+package com.techie.corejava;
+
+public class MethodAndParameterScope {
+
+	static int x = 10;
+	private int y = 20;
+	int z;
+
+	public void testFunc(int z) {
+
+		MethodAndParameterScope mp = new MethodAndParameterScope();
+		this.x = 30;
+		y = 40;
+		this.y = 50;
+
+		System.out.println("MethodAndParameterScope.x is : " + MethodAndParameterScope.x);
+		System.out.println("mp.x : " + mp.x);
+		System.out.println("mp.y : " + mp.y);
+		System.out.println("y : " + y);
+		
+		System.out.println("before z : " + this.z);
+		this.z = z;
+		System.out.println("After z : " + z);
+	}
+
+	public static void main(String[] args) {
+		MethodAndParameterScope mpObj = new MethodAndParameterScope();
+		mpObj.testFunc(100);
+	}
+
+}
